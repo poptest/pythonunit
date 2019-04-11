20827db7383dc5c8e1617d3b66006af28c5108ca
407081518 <mehuandiguo@126.com>
2019-04-11 17:55:26 +0800

    增加聚合数据-电话归属地相关测试的 1. 封装业务断言

---
+++ junyi/return_value.py
@@ -0,0 +1,18 @@
+
+def return_1_value():
+    a = 1
+    return a
+
+
+ret_val = return_1_value()
+print(ret_val)
+
+
+def return_2_value():
+    a = 1
+    b = 2
+    return a, b
+
+
+ret_val_2_1, ret_val_2_2 = return_2_value()
+print(ret_val_2_1, ret_val_2_2)
--- junyi/utils/tellocation.py
+++ junyi/utils/tellocation.py
@@ -38,3 +38,52 @@ class TelLocationUtil():
 
         #4. 返回组装好的字典
         return ret_dic
+
+    def tel_location_assert(self, resp, exp_status_code, exp_provice, exp_city, exp_areacode, exp_zip, exp_company,
+                            exp_card):
+        assert_result_pass = []
+        assert_result_fail = []
+
+        act_statu_code = resp[TelLocationUtil.STATUS_CODE]
+        if act_statu_code == exp_status_code:
+            assert_result_pass.append({"statu_code": [act_statu_code, exp_status_code]})
+        else:
+            assert_result_fail.append({"statu_code": [act_statu_code, exp_status_code]})
+
+        act_provice = resp[TelLocationUtil.PROVINCE]
+        if act_provice == exp_provice:
+            assert_result_pass.append({"provice": [act_provice, exp_provice]})
+        else:
+            assert_result_fail.append({"provice": [act_provice, exp_provice]})
+
+        act_city = resp[TelLocationUtil.CITY]
+        if act_city == exp_city:
+            assert_result_pass.append({"city": [act_city, exp_city]})
+        else:
+            assert_result_fail.append({"city": [act_city, exp_city]})
+
+        act_areacode = resp[TelLocationUtil.AREACODE]
+        if act_areacode == exp_city:
+            assert_result_pass.append({"areacode": [act_areacode, exp_areacode]})
+        else:
+            assert_result_fail.append({"areacode": [act_areacode, exp_areacode]})
+
+        act_zip = resp[TelLocationUtil.ZIP]
+        if act_zip == exp_zip:
+            assert_result_pass.append({"zip": [act_zip, exp_zip]})
+        else:
+            assert_result_fail.append({"zip": [act_zip, exp_zip]})
+
+        act_company = resp[TelLocationUtil.COMPANY]
+        if act_company == exp_company:
+            assert_result_pass.append({"zip": [act_company, exp_company]})
+        else:
+            assert_result_fail.append({"zip": [act_company, exp_company]})
+
+        act_card = resp[TelLocationUtil.CARD]
+        if act_card == exp_card:
+            assert_result_pass.append({"card": [act_card, exp_card]})
+        else:
+            assert_result_fail.append({"card": [act_card, exp_card]})
+
+        return assert_result_pass, assert_result_fail
