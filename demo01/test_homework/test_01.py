7626d987316c3fd5bbf1f5f975bf8e9c31315339
qsong <qsong.vip@qq.com>
2019-04-25 20:43:46 +0800

    DDT

---
+++ demo01/test_homework/test_01.py
@@ -0,0 +1,25 @@
+# -*- encoding:utf-8 -*-
+import unittest
+
+from junyi.homework.lrjs import LiRunJiSuan
+from ddt import ddt, data, unpack
+
+
+@ddt
+class MyTestCase(unittest.TestCase):
+
+    @data({"lr":10, "jj":9},
+          {"lr":20, "jj":9},
+          {"lr":30, "jj":10})
+    @unpack
+    def test_case01(self, lr, jj):
+        #1. 准备测试数据
+        lr = lr
+        #2. 执行测试步骤：调用利润计算去计算
+        act_jj = LiRunJiSuan.jisuan(lr)
+        #3. 定义期望值
+        exp_jj = jj
+        self.assertEqual(exp_jj, act_jj)
+
+if __name__ == '__main__':
+    unittest.main()
