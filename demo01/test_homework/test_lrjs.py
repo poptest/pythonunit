efcd1afc70033f393f205181117deafd81e26922
qsong <qsong.vip@qq.com>
2019-04-25 19:22:15 +0800

    update homework

---
+++ demo01/test_homework/test_lrjs.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+import unittest
+
+from junyi.homework.lrjs import LiRunJiSuan
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        #1. 准备测试数据
+        zlr = 10
+        #2. 运行测试步骤，并获取实际结果
+        act_jj = LiRunJiSuan.jisuan(zlr)
+        #3. 定义期望结果
+        exp_jj = 1
+        #4. 判断期望奖金与实际奖金是否相等
+        self.assertEqual(exp_jj, act_jj)
+
+
+if __name__ == '__main__':
+    unittest.main()
