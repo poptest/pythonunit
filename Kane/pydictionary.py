6b861dd81ffe3dc60b39106a4a5c57ea54b42827
KaneQin <352093042@qq.com>
2019-04-26 11:46:50 +0800

    字典取出数据，并拼接字符串

--- Kane/pydictionary.py
+++ Kane/pydictionary.py
@@ -19,7 +19,7 @@ dic = {
 # 电话号码:13429667914, 省份：浙江， 城市：杭州， 区域编码：0571， 邮编：310000， 运营商：移动
 #往字典里面添加相应的数据
 dic["result"]["tel"] = 13429667914
-dic["result"]["name"] = "Kane"
+dic["result"]["name"] = "Kane","Alley"
 
 #根据要求取出相应的数据
 name = dic["result"]["name"]
@@ -31,5 +31,5 @@ company = dic["result"]["company"]
 tel = dic["result"]["tel"]
 
 #拼接字符串，并打印输出
-str="电话号码：%s，姓名：%s，省份：%s，城市：%s，区域编码：%s，邮编：%s，运营商：%s"%(tel, name, province, city, areacode, zip, company)
+str="电话号码：%s\r\n姓名：%s\r\n省份：%s\r\n城市：%s\r\n区域编码：%s\r\n邮编：%s\r\n运营商：%s"%(tel, name, province, city, areacode, zip, company)
 print (str)
