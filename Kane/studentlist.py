4657ac1d1231bf8f1a4762a5fa8dae1b2aaeacd7
KaneQin <352093042@qq.com>
2019-04-26 14:23:22 +0800

    定义一个方法，计算女生的数量

--- Kane/studentlist.py
+++ Kane/studentlist.py
@@ -7,13 +7,24 @@ sudent_list=[
     {"name":"Lily", "age":"12", "sex":"nv"},
     {"name":"Alex", "age":"12", "sex":"nan"},
 ]
-count=0
-#通过for循环遍历list中的字典，并通过相应的key等到相应的val
-for student in sudent_list:
-    studnet_name=student["name"]
-    print ("name:%s"%studnet_name)
-    #判断性别是否为女
-    if(student["sex"]=="nv"):
-        count+=1
+# count=0
+# #通过for循环遍历list中的字典，并通过相应的key等到相应的val
+# for student in sudent_list:
+#     studnet_name=student["name"]
+#     print ("name:%s"%studnet_name)
+#     #判断性别是否为女
+#     if(student["sex"]=="nv"):
+#         count+=1
+#
+# print ("一共有%s个女生"%count)
 
-print ("一共有%s个女生"%count)
+#定义一个方法，实现计算女生的个数
+def count_nv(students):
+    count=0
+    for student in students:
+        if student["sex"] == "nv":
+            count+=1
+    return count
+
+student_nv=count_nv(sudent_list)
+print ("一共有%s个女生"%student_nv)
